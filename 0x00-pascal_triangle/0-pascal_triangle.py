c478fce5979e02a2723de6fcc9bfe79331a9a50c
Iyadi Cyril <iyadicy@gmail.com>
2022-11-06 13:01:40 +0100

    refactor: add comments

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle returns an array of array
+each sub-array represents a row in the pascal triangle
+representation of the number passed in as an argument.
+"""
+
 
 def factorial(n):
+    """
+    Calculate the factorial
+    """
     if n <= 1:
         return 1
     else:
@@ -8,10 +17,16 @@ def factorial(n):
 
 
 def combination(n, r):
+    """
+    Calculate nCr ie n combination r
+    """
     return int(factorial(n) / (factorial(r) * factorial(n - r)))
 
 
 def generate_row(n):
+    """
+    Generates and returns row i of the pascal's triangle
+    """
     array = [1]
     if n == 2:
         array.append(1)
@@ -21,6 +36,9 @@ def generate_row(n):
 
 
 def compute_coefficients(n):
+    """
+    Generates and returns the row i of the pascal's triangle as an array
+    """
     array = []
     stop_index = int((n / 2) + 1) if ((n % 2) == 0) else int(((n + 1) / 2) + 1)
     for i in range(1, stop_index):
@@ -33,6 +51,9 @@ def compute_coefficients(n):
 
 
 def pascal_triangle(n):
+    """
+    Generates the entire pascal's triangle of an as an array of arrays
+    """
     result = [""]
 
     if n >= 1:
