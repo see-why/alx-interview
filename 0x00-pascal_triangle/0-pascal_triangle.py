a62e864908dc23cd38ba1ea72f802368437c0b70
Iyadi Cyril <iyadicy@gmail.com>
2022-11-19 10:19:47 +0100

    refactor: use simpler loop for solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,59 +6,19 @@ representation of the number passed in as an argument.
 """
 
 
-def factorial(n):
-    """
-    Calculate the factorial
-    """
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-def combination(n, r):
-    """
-    Calculate nCr ie n combination r
-    """
-    return (factorial(n) / (factorial(r) * factorial(n - r)))
-
-
-def generate_row(n):
-    """
-    Generates and returns row i of the pascal's triangle
-    """
-    array = [1]
-    if n == 2:
-        array.append(1)
-    elif n > 2:
-        array += compute_coefficients(n - 1) + [1]
-    return array
-
-
-def compute_coefficients(n):
-    """
-    Generates and returns the row i of the pascal's triangle as an array
-    """
-    array = []
-    stop_index = int((n / 2) + 1) if ((n % 2) == 0) else int(((n + 1) / 2) + 1)
-    for i in range(1, stop_index):
-        com = combination(n, i)
-        array.append(str(com).split('.')[0])
-
-    reversed = array[::-1]
-
-    result = array + reversed[1:] if n % 2 == 0 else array + reversed[2:]
-    return result
-
-
 def pascal_triangle(n):
     """
     Generates the entire pascal's triangle of an as an array of arrays
     """
     result = []
 
-    if n >= 1:
-        for i in range(1, n + 1):
-            result.append(generate_row(i))
+    for i in range(n):
+        row = [1]
+        result.append(row)
+        for j in range(1, i):
+            next_coefficient = result[i - 1][j - 1] + result[i - 1][j]
+            row.append(next_coefficient)
+        if i >= 1:
+            row.append(1)
 
-    return result
+    return result if n > 0 else []
